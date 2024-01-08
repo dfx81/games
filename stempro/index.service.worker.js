e77c0b53b3e5bb0b0c62c766c66f4c9ccce5a576
Danial Fitri <danialfitrighazali@gmail.com>
2024-01-08 22:08:32 +0800

    Improved resolution

--- stempro/index.service.worker.js
+++ stempro/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1704720242|85042729592";
+const CACHE_VERSION = "1704722699|87499650893";
 const CACHE_PREFIX = "Stem Pro-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
