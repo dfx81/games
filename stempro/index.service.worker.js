40246a6a40d072a1aa0cb4ebbbb7b242fd8439fc
Danial Fitri <danialfitrighazali@gmail.com>
2024-02-13 23:36:27 +0800

    Add files via upload

--- stempro/index.service.worker.js
+++ stempro/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1707757972|7029769592";
+const CACHE_VERSION = "1707838303|2343251959";
 const CACHE_PREFIX = "STEMPro-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
