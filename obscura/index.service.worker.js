89ccd3dce0c60927adc47c1bbc8943a2fad9d4b7
Danial Fitri <danialfitrighazali@gmail.com>
2026-03-18 15:05:06 +0800

    Fix timing bug on obscura

--- obscura/index.service.worker.js
+++ obscura/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773810616|76202041658';
+const CACHE_VERSION = '1773817323|722635478';
 /** @type {string} */
 const CACHE_PREFIX = 'obscura-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
