668751daaa2acd5cf8f2f2915fde7a0ce9f26ea9
dfx81 <danialfitrighazali@gmail.com>
2026-06-02 11:54:38 +0800

    Updated pong

--- pong/index.service.worker.js
+++ pong/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780339915|44464061809';
+const CACHE_VERSION = '1780372135|8017131721';
 /** @type {string} */
 const CACHE_PREFIX = 'Pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
